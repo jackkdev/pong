88db62862bec303577da189fbcfc3f2eda12de34
jackk <j4ckk3@gmail.com>
2022-01-25 16:09:00 -0600

    initial commit

---
+++ pong/src/gfx/window.h
@@ -0,0 +1,11 @@
+#ifndef PONG_PONG_SRC_GFX_WINDOW_H_
+#define PONG_PONG_SRC_GFX_WINDOW_H_
+
+#define GLFW_INCLUDE_NONE
+#include <GLFW/glfw3.h>
+
+typedef struct {
+  GLFWwindow *handle_;
+} window_t;
+
+#endif //PONG_PONG_SRC_GFX_WINDOW_H_
---
+++ pong/src/pch.h
@@ -0,0 +1,13 @@
+#ifndef PONG_PONG_SRC_PCH_H_
+#define PONG_PONG_SRC_PCH_H_
+
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdio.h>
+
+#include <string.h>
+#include <memory.h>
+
+#include "util/numbers.h"
+
+#endif //PONG_PONG_SRC_PCH_H_
